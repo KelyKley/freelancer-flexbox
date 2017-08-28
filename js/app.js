5ffeac5ce82248a96ee06f2ac3ecac10fc5d177f
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-08-28 10:03:48 -0500

    terminado

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/*
+ * Archivo principal de JS
+ */
+
+ function zoom(){
+ 	document.getElementById("contenido").style.display='block';
+ }
+
+ function zoom2(){
+ 	document.getElementById("contenido2").style.display='block';
+ }
+
+ function zoom3(){
+ 	document.getElementById("contenido3").style.display='block';
+ }
+
+  function zoom4(){
+ 	document.getElementById("contenido4").style.display='block';
+ }
+
+  function zoom5(){
+ 	document.getElementById("contenido5").style.display='block';
+ }
+
+  function zoom6(){
+ 	document.getElementById("contenido6").style.display='block';
+ }
+
+ function zoom7(){
+ 	document.getElementById("contenido7").style.display='block';
+ }
+
+  function zoom8(){
+ 	document.getElementById("contenido8").style.display='block';
+ }
+
+  function zoom9(){
+ 	document.getElementById("contenido9").style.display='block';
+ }
